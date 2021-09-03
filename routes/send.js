056d4c048421e79cd88296a127e257caddcf8057
Phillip Stene <phil.stene@gmail.com>
2021-09-03 17:59:20 -0400

    display a drop down if any users found, otherwise a textfield for recipient on send page

--- routes/send.js
+++ routes/send.js
@@ -7,10 +7,19 @@ const router = express.Router();
 router.get('/send', secured(), (req, res) => {
     UserModel.find({}).sort({date: 'desc'}).exec((err, records) => {
         if(err) console.error(err);
-        let users = records.reduce((acc, curr) => {
-            return acc + '<option value="' + curr.name + '">' + curr.name + '</option>';
-        }, '');
-        res.render('send', { data: users, page: 'send' });
+        let recipients = '';
+        // if(records.length > 0) {
+        if(false) {
+            recipients += '<select name="recipient" id="recipient" required><option value="">Select recipient</option>';
+            recipients += records.reduce((acc, curr) => {
+                return acc + '<option value="' + curr.name + '">' + curr.name + '</option>';
+            }, '');
+            recipients += '</select>';
+        } else {
+            recipients += '<input id="recipient" name="recipient" type="text" placeholder="Ex: abcd@defg.com" pattern="email" required/>';
+        }
+
+        res.render('send', { recipients: recipients, page: 'send' });
     });
 });
 
