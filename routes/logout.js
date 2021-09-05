df9277ba3922efd290fd283e37c19c5b0f5e5c30
Phillip Stene <phil.stene@gmail.com>
2021-09-04 23:12:33 -0400

    removed port inclusion in returnTo string in logout when in production

--- routes/logout.js
+++ routes/logout.js
@@ -9,32 +9,31 @@ const querystring = require('querystring');
 dotenv.config();
 
 router.get('/lgo', (req, res) => {
-   console.log('before req.logout()');
-   req.logout();
-   console.log('after req.logout()');
-
    let returnTo = req.protocol + '://' + req.hostname;
-   console.log('returnTo = ' + returnTo);
    const port = req.connection.localPort;
-   console.log('port = ' + port);
    if (port !== undefined && port !== 80 && port !== 443) {
-      returnTo += ':' + port;
+      returnTo = process.env.NODE_ENV === 'production' ? `${returnTo}/logout` : `${returnTo}:${port}/logout`;
    }
-   returnTo += '/logout';
-   console.log('returnTo(with port) = ' + returnTo);
 
-   let logoutURL = new url.URL(
-       util.format('https://%s/v2/logout', process.env.AUTH0_DOMAIN)
-   );
+   req.logout();
 
-   const searchString = querystring.stringify({
-      client_id: process.env.AUTH0_CLIENT_ID,
-      returnTo: returnTo
-   });
-   logoutURL.search = searchString;
-   console.log('logoutURL = ' + logoutURL);
+   if(req.session) {
+      req.session.destroy( (err) => {
+         if(err) console.error(err);
 
-   res.redirect(logoutURL);
+         let logoutURL = new url.URL(
+             util.format('https://%s/v2/logout', process.env.AUTH0_DOMAIN)
+         );
+
+         const searchString = querystring.stringify({
+            client_id: process.env.AUTH0_CLIENT_ID,
+            returnTo: returnTo
+         });
+         logoutURL.search = searchString;
+
+         res.redirect(logoutURL);
+      });
+   }
 });
 
 router.get('/logout' , (req, res) => {
